31a8570033b7dae391cc29a45c409a928a0f194e
Renat Rysaev <renat.rysaev@gmail.com>
2018-12-24 00:52:47 +0500

    [issue-26] Add prettier

--- src/app.js
+++ src/app.js
@@ -2,9 +2,8 @@ import express from 'express'
 
 const app = express()
 
-const port = 8000;
+const port = 8000
 
 app.listen(port, () => {
-  console.log('Server started on port:' + port);
-});
-
+  console.log('Server started on port:' + port)
+})
